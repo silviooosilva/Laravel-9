52333227df2c1676ad687efab4b470e7059541db
silviooosilva <gasparsilvio7@gmail.com>
2022-08-18 13:27:40 +0100

    Rodando o seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,11 +19,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UsersSeeder::class
+        ]);
     }
+
+    /**
+     *
+     * Agora basta rodar o nosso seed!
+     *
+     * php artisan db:seed
+     *
+     */
 }
